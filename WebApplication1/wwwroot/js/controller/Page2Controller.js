f7c69dc9422facd5f5c9c2a631924dc5a8e6c4a5
niloofar <n.mahzali70@gmail.com>
2018-04-11 21:59:57 +0430

    page2

---
+++ WebApplication1/wwwroot/js/controller/Page2Controller.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('page2Controller', page2Controller);
+
+    page2Controller.$inject = ['$scope', '$http'];
+
+    function page2Controller($scope, $http) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'controller';
+
+        $http.get('./api/Values/api/array').then(
+            function (response) {
+                $scope.val3 = response.data;
+            },
+            function (error) {
+                console.log(error);
+            });
+    }
+})();
